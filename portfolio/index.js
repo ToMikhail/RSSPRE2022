909f8c5d74dbc134a9c6b2635b162961e366aba7
Toustsik Mikahil <toustsikmikhail@gmail.com>
2022-01-17 12:26:25 +0100

    feat: add js and ico

---
+++ portfolio/index.js
@@ -0,0 +1,35 @@
+"use strict";
+console.log(`your score is - 110 points.
+Отзывы по пунктам ТЗ:
+Все пункты выполнены полностью!`);
+
+/* function for checking semantic */
+// const elemsCount = (selector) => document.querySelectorAll(selector).length;
+// const predicate = (configItem) => {
+//     const count = elemsCount(configItem[0]);
+//     return count >= configItem[1] && count <= configItem[2];
+// };
+// const evaluate = (isFullfilled) => (isFullfilled ? 2 : 0);
+// const isArray = (item) => Array.isArray(item[0]);
+// const config = [
+//     [
+//         ['header', 1, 1],
+//         ['main', 1, 1],
+//         ['footer', 1, 1],
+//     ],
+//     ['section', 6, Infinity],
+//     ['h1', 1, 1],
+//     ['h2', 5, Infinity],
+//     ['nav', 1, Infinity],
+//     ['ul', 2, Infinity],
+//     ['button', 10, Infinity],
+//     [
+//         ['input[type="email"]', 1, Infinity],
+//         ['input[type="tel"]', 1, Infinity],
+//     ],
+//     ['textarea', 1, Infinity],
+//     ['input, textarea[placeholder]', 3, Infinity],
+// ];
+// config.reduce((sum, configItem) => sum + (isArray(configItem)
+//     ? evaluate(configItem.every(predicate))
+//     : evaluate(predicate(configItem))), 0);
