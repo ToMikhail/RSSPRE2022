92dfec969dabd6327979a94f13b6e1cacfe8ac9d
Toustsik Mikahil <toustsikmikhail@gmail.com>
2022-02-06 12:42:06 +0100

    feat: add func  translate

--- eco-sounds/index.js
+++ eco-sounds/index.js
@@ -1,23 +1,40 @@
+import lang from "./translate.js";
 const playBtn = document.querySelector(".btn-play");
 const audio = document.querySelector(".audio");
-const birdBtns = document.querySelector('.nav-items')
-const birdNamesBtn = document.querySelectorAll('[data-name]') 
+const birdBtns = document.querySelector(".nav-items");
+const birdNamesBtn = document.querySelectorAll("[data-name]");
+const langBtn = document.querySelector('.lang')
 
 let isPlay = false;
-let image = document.getElementsByClassName('пшет main-container')
+let image = document.querySelector(".main-container");
+// (birdNamesBtn[0].childNodes[1].textContent = 'asdasd')
 
-console.log(image);
+// console.log(lang['en']);
 
-/* change PLAY/PAUSE */
-playBtn.addEventListener("click", () => {
-  playBtn.classList.toggle("pause");
-  if(!isPlay){
-    playMusic();
+
+
+/* change lang */
+
+langBtn.addEventListener('click', () => {
+  langBtn.classList.toggle('russian');
+
+  if(langBtn.classList.contains('russian')) {
+    langBtn.textContent = 'RU';
   }
   else {
-    pauseMusic();
+    langBtn.textContent = 'EN';
   }
-});
+  changeLang()
+})
+
+/* func change lang content */
+function changeLang() {
+  let currentLang = langBtn.textContent
+  birdNamesBtn.forEach(element => {
+    let name = element.dataset.name;
+    element.textContent = lang[currentLang][name]
+  });
+}
 
 /* Music */
 function playMusic() {
@@ -31,18 +48,32 @@ function pauseMusic() {
   isPlay = false;
 }
 
+/* change PLAY/PAUSE */
+playBtn.addEventListener("click", () => {
+  playBtn.classList.toggle("pause");
+  if (!isPlay) {
+    playMusic();
+  } else {
+    pauseMusic();
+  }
+});
+
 /* change picture */
 
-birdBtns.addEventListener('click', (event) => {
-  birdNamesBtn.forEach(element => {
-    if(element.classList.contains('active')) {
-      element.classList.remove('active')
+birdBtns.addEventListener("click", (event) => {
+  birdNamesBtn.forEach((element) => {
+    if (element.classList.contains("active")) {
+      element.classList.remove("active");
     }
   });
-  event.target.closest('li').classList.add('active');
-  let birdImg = event.target.closest('li').dataset.name;
-  image.style.background = "assets/img/drozd.jpg";
-  console.log('image: ', image.style);
-})
+  event.target.closest("li").classList.add("active");
+  let birdImg = event.target.closest("li").dataset.name;
+  image.style.backgroundImage = `url(./assets/img/${birdImg}.jpg)`;
+  audio.src = `./assets/audio/${birdImg}.mp3`;
+  if (playBtn.classList.contains("pause")) audio.play();
+});
 
-
+console.log(`Ваша отметка - 70 балла(ов)
+Отзыв по пунктам ТЗ:
+Все пункты выполнены полностью!
+`);
---
+++ eco-sounds/translate.js
@@ -0,0 +1,18 @@
+const lang = {
+  'RU': {
+    'solovey': 'Соловей',
+    'drozd':'Дрозд',
+    'zarynka': 'Малиновка',
+    'javoronok': 'Жаворонок',
+    'slavka': 'Славка'
+  },
+  'EN': {
+    'solovey': 'Nightingale',
+    'drozd':'Blackbird',
+    'zarynka': 'Robin',
+    'javoronok': 'Lark',
+    'slavka': 'Warbler'
+  }
+}
+
+export default lang;
